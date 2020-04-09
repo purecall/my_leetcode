2eb748713b5eb90a6f8aebae029e423c14e4b7bb
purecallxu <purecallxu@tencent.com>
2020-04-09 21:42:35 +0800

    add 104

---
+++ 104 二叉树的最大深度.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=104 lang=python3
+#
+# [104] 二叉树的最大深度
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        # 递归，二叉树的最大深度==max(左子树最大深度，右子树最大深度)+1
+        if not root:
+            return 0
+        left_height = self.maxDepth(root.left)
+        right_height = self.maxDepth(root.right)
+        return max(left_height, right_height)+1
+# @lc code=end
