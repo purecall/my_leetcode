f491a8390ec202e7167cfdff803228dc158a441f
purecallxu <purecallxu@tencent.com>
2020-04-26 17:15:50 +0800

    modify 322

--- 322 零钱兑换.py
+++ 322 零钱兑换.py
@@ -10,7 +10,7 @@ class Solution:
         dp = [float("inf") for i in range(amount+1)] # 初始化dp数组
         dp[0] = 0 # 当amount==0时，硬币所需数为0
 
-        # 举个例子 dp[10]= min(dp[10-5], dp[10-8], dp[10-1]) + 1
+        # 举个例子 dp[10]= min(dp[10-5], dp[10-2], dp[10-1]) + 1
         for i in range(1,amount+1):
             for coin in coins:
                 if i - coin >= 0:
