39ab9b2ad3255d7abf77522e66a59853f041aa90
purecallxu <purecallxu@tencent.com>
2020-04-27 16:11:07 +0800

    modify 198

--- 198 打家劫舍.py
+++ 198 打家劫舍.py
@@ -18,6 +18,9 @@ class Solution:
         for i in range(1,len(nums)):
             dp[i][0] = max(dp[i-1][0],dp[i-1][1])
             dp[i][1] = dp[i-1][0] + nums[i]
+            
         return max(dp[-1][0], dp[-1][1])
+
+        # 第二种dp, dp[i] = max(dp[i-1], dp[i-2]+nums[i]) 不是很好理解
 # @lc code=end
 
