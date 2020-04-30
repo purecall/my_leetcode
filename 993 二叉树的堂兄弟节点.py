a0ad3a97f2acb2eaffd7db092c10933132d81f64
purecallxu <purecallxu@tencent.com>
2020-04-30 22:09:52 +0800

    add 993 Solution 2

--- 993 二叉树的堂兄弟节点.py
+++ 993 二叉树的堂兄弟节点.py
@@ -29,3 +29,24 @@ class Solution:
         return depth[x]==depth[y] and parent[x]!=parent[y]
 # @lc code=end
 
+
+
+# 迭代->层次遍历，用info_dict存储每个node.val的parent和depth信息
+class Solution1:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        helper = [root]
+        info_dict = { root.val: (None,0) }
+        depth = 1
+        while helper:
+            new_helper = []
+            for node in helper:
+                if node.left:
+                    info_dict[node.left.val] = (node, depth)
+                    new_helper.append(node.left)
+                if node.right:
+                    info_dict[node.right.val] = (node, depth)
+                    new_helper.append(node.right)
+            helper = new_helper
+            depth += 1
+
+        return info_dict[x][0] != info_dict[y][0] and info_dict[x][1] == info_dict[y][1]
