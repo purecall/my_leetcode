18d1542b36a87ebcf7d88a7899361bd0ae30fa4b
purecallxu <purecallxu@tencent.com>
2020-05-01 13:39:44 +0800

    add 21

---
+++ 21 合并两个有序链表.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# 递归
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+# @lc code=end
+
+
+# 迭代
+class Solution1:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode(-1)
+
+        cur = head
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        
+        # 合并后l1和l2最多只有一个还未被合并完
+        # 直接将链表末尾元素指向未合并完的链表即可
+        cur.next = l1 if l1 else l2
+        return head.next
