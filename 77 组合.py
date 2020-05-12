630c5890e465bfee59378377835b771a88af602c
purecallxu <purecallxu@tencent.com>
2020-05-12 10:23:57 +0800

    add 77

---
+++ 77 组合.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=77 lang=python3
+#
+# [77] 组合
+#
+
+# @lc code=start
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        output = []
+
+        def backtrack(first=1, curr=[]):
+            if len(curr) == k:
+                output.append(curr[:])
+            for i in range(first, n+1):
+                curr.append(i)
+                backtrack(i+1, curr)
+                curr.pop()
+
+        backtrack()
+        return output
+        # @lc code=end
