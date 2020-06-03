cc77ea6787644cd5e10e6c42dcede9cbe4572d85
purecallxu <purecallxu@tencent.com>
2020-06-03 21:44:33 +0800

    add 189

---
+++ 189 旋转数组.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=189 lang=python3
+#
+# [189] 旋转数组
+#
+
+# @lc code=start
+class Solution:
+    # 三次旋转即可
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        self.reverse(nums, 0, len(nums)-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, len(nums)-1)
+        return 
+
+
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+        return
+# @lc code=end
+
