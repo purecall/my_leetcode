f1b79d6521283fec543c11b663f7b3e181b5ceec
purecallxu <purecallxu@tencent.com>
2020-06-05 11:30:32 +0800

    add 137

---
+++ 137 只出现一次的数字2.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode.cn id=137 lang=python3
+#
+# [137] 只出现一次的数字 II
+#
+
+# @lc code=start
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        from collections import Counter
+        counts = Counter(nums)
+        for k, v in counts.items():
+            if v == 1:
+                return k
+# @lc code=end
+
+
+# 位运算，没看懂...
+class Solution1:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen_once = seen_twice = 0
+        
+        for num in nums:
+            # first appearance: 
+            # add num to seen_once 
+            # don't add to seen_twice because of presence in seen_once
+            
+            # second appearance: 
+            # remove num from seen_once 
+            # add num to seen_twice
+            
+            # third appearance: 
+            # don't add to seen_once because of presence in seen_twice
+            # remove num from seen_twice
+            seen_once = ~seen_twice & (seen_once ^ num)
+            seen_twice = ~seen_once & (seen_twice ^ num)
+
+        return seen_once
