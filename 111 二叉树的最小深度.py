0af16e74c9a279e14ca93e9b0441635a2a66f3d3
purecallxu <purecallxu@tencent.com>
2020-06-07 22:26:39 +0800

    add 111

--- 111 二叉树的最小深度.py
+++ 111 二叉树的最小深度.py
@@ -15,7 +15,7 @@
 
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
-        if not root:
+        if not root: 
             return 0
         left = self.minDepth(root.left)
         right = self.minDepth(root.right)
