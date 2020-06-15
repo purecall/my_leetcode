e3fc5e6826f2f2f530ffd81382703da68c15b7c2
purecallxu <purecallxu@tencent.com>
2020-06-15 21:11:45 +0800

    add 28

---
+++ 28 实现strStr().py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=28 lang=python3
+#
+# [28] 实现 strStr()
+#
+
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                return i    
+        return -1
+
+# @lc code=end
+
