067451c8ae75f82af9446c2de7e45bae882fbe5d
purecallxu <purecallxu@tencent.com>
2020-07-06 01:05:55 +0800

    add 226

---
+++ 226 翻转二叉树.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=226 lang=python3
+#
+# [226] 翻转二叉树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        if not root.left and not root.right:
+            return root
+
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
+# @lc code=end
